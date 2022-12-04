15da8db5de714b1d739ff24155a3d375a22487a4
Dexter Dick <@dexterdick@hotmail.com>
2022-12-04 09:56:20 -0500

    added questions in utils and made updates to index.js

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
 const inquirer = require("inquirer");
-
-// TODO: Create an array of questions for user input
-const questions = [
-    {
-        name: "project_title",
-        type: "input",
-        message: "Enter your project title",
-    },
-    {
-        name: "description",
-        type: "input",
-        message: "Enter project description",
-    },
-    {},
-];
+const markDownJs = require("./utils/generateMarkdown.js");
+const questions = require("./utils/questions.js");
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            // Use user feedback for... whatever!!
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else went wrong
+            }
+        });
+}
 
 // Function call to initialize app
 init();
