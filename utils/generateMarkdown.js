804cacec3f27ac59cdeeb92f5c9421ae6b1430e8
Dexter Dick <@dexterdick@hotmail.com>
2022-12-09 23:29:13 -0500

    made updates to markdown function and renderLicense

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license === "DSD") {
+        return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+    } else if (license === "MIT") {
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    } else if (license === "GNU GPL") {
+        return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,12 +20,7 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.project_title}\n\n## ${data.description}\n\n## ${data.table_of_contents}\n\n## ${data.usage}\n\n## ${data.contributing}\n\n## ${data.tests}\n\n## ${data.questions}
-    
-    
-    
-    
-    `;
+    return `# ${data.project_title}\n\n## Description\n\n${data.description}\n\n## Table of Contents\n\n 1. [Installation](#installation)\n\n 2. [Usage](#usage)\n\n 3. [License](#license) \n\n 4. [Contributing](#contributing)\n\n ## Installation\n\n${data.installation}\n\n## Usage\n\n${data.usage}\n\n## License\n\n${data.license}\n\n## Contributing\n\n${data.contributing}\n\n## Test\n\n${data.tests}\n\n## Questions\n\n${data.questions}`;
 }
 
 module.exports = generateMarkdown;
